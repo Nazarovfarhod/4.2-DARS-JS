ea36d17d32e563c451db3f3d08e56b3485c2f86a
farxodnazarov <farhodnazarov002gmail.com>
2024-04-19 14:57:05 +0500

    added some independent ideas

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // prompt
 
-let ism = prompt('Ismingizni kiriting')
-
-alert( `Salom, ${ism.charAt().toUpperCase()}${ism.slice(1).toLowerCase()}`);
+let firstName = prompt("Ismingizni kiriting:");
+let lastName = prompt("Familyangizni kiriting:");
+alert(
+  `Salom,  ${lastName.charAt().toUpperCase()}${lastName
+    .slice(1)
+    .toLowerCase()} ${firstName.charAt().toUpperCase()}${firstName
+    .slice(1)
+    .toLowerCase()} Saytimizga hush kelibsiz!`
+);
